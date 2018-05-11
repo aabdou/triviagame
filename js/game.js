bac011cc963b5814f7b61798c9b4952b3b6c112f
Amir Abdou <amisaad@gmail.com>
2018-05-10 21:20:08 -0700

    add logic to the play button

--- js/game.js
+++ js/game.js
@@ -1,27 +1,50 @@
 // Store references to the UI
-var playButton = document.getElementById("playButton");
-var answerButton = document.getElementById("answerButton");
-var questionDiv = document.getElementById("question");
-var remainingDiv = document.getElementById("remaining");
-var correctDiv = document.getElementById("correct");
+let playButton = document.getElementById("playButton");
+let answerButton = document.getElementById("answerButton");
+let questionDiv = document.getElementById("question");
+let remainingDiv = document.getElementById("remaining");
+let correctDiv = document.getElementById("correct");
 
 // Game state
-var questions = [];
-var currentQuestionIndex = -1;
-var correctAnswers = 0;
-var remainingQuestions = 10;
+let questions = [];
+let currentQuestionIndex = -1;
+let correctAnswers = 0;
+const NumOfQuestions = 10;
 
-function DisplayQuestion()
+playButton.addEventListener("click", function() {
+  // 1. Fetch the questions from the Trivia API
+  fetch("https://opentdb.com/api.php?type=boolean&amount=" + NumOfQuestions)
+  .then(response => response.json()) 
+  .then(function(jsonRes) {
+    if (jsonRes.response_code != 0) // OOps, we have an error!
+    {
+      console.log("Error retrieving questions from the API");
+    }
+    else // else
+    {
+      // a. Store the questions in our variable
+      questions = jsonRes.results;
+
+      // b. To display the first question we set the following to zero
+      currentQuestionIndex = 0;
+
+      // c. Displays the current question
+      DisplayCurrentQuestion();
+
+      // d. hide play button and show the answer button
+      playButton.style.display = "none";
+      answerButton.style.display = "block";
+    }
+  });
+});
+
+answerButton.addEventListener("click", function() {
+
+});
+
+function DisplayCurrentQuestion()
 {
-
+  // TODO: Implement this function
+  var questionObj = questions[currentQuestionIndex];
+  console.log(questionObj.question);
 }
-
-playButton.onclick = function()
-{
-
-};
-
-answerButton.onclick = function()
-{
-
-};
