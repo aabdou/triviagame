2307dcca97169bb6bb5dde2642530ef0d36e25af
Amir Abdou <amisaad@gmail.com>
2018-05-10 20:38:53 -0700

    initial files

---
+++ js/game.js
@@ -0,0 +1,27 @@
+// Store references to the UI
+var playButton = document.getElementById("playButton");
+var answerButton = document.getElementById("answerButton");
+var questionDiv = document.getElementById("question");
+var remainingDiv = document.getElementById("remaining");
+var correctDiv = document.getElementById("correct");
+
+// Game state
+var questions = [];
+var currentQuestionIndex = -1;
+var correctAnswers = 0;
+var remainingQuestions = 10;
+
+function DisplayQuestion()
+{
+
+}
+
+playButton.onclick = function()
+{
+
+};
+
+answerButton.onclick = function()
+{
+
+};
